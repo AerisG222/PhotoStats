4a7670565b55a19d784d1f54d2c9c6f1da600065
Mike Morano <mmorano@mikeandwan.us>
2017-01-07 16:08:52 -0500

    cleanup / travis

--- src/PhotoStats/PhotoStatsOptions.cs
+++ src/PhotoStats/PhotoStatsOptions.cs
@@ -1,20 +1,46 @@
-using System.Collections.Generic;
-using CommandLine;
+using System;
+using System.CommandLine;
 
 
 namespace PhotoStats
 {
     public class PhotoStatsOptions
     {
-        [Option('p', "photo-dir", HelpText = "Directory containing the source photos")]
-        public string PhotoRoot { get; set; }
+        bool _help;
+        string _photoRoot;
+
+        public string PhotoRoot { get { return _photoRoot; } }
+
+
+        public void Parse(string[] args)
+        {
+            ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.ApplicationName = "SizePhotos";
+
+                syntax.HandleHelp = false;
+
+                syntax.DefineOption("h|help", ref _help, "help");
+                syntax.DefineOption("p|photo-dir", ref _photoRoot, "Directory containing the source photos");
+
+                if(_help)
+                {
+                    Console.WriteLine(syntax.GetHelpText());
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    ValidateOptions(syntax);
+                }
+            });
+        }
+
         
-        
-        public IEnumerable<string> ValidateOptions()
+        void ValidateOptions(ArgumentSyntax syntax)
         {
             if(string.IsNullOrWhiteSpace(PhotoRoot))
             {
-                yield return "Please specify the local path containing the photos to process";
+                syntax.ReportError("Please specify the local path containing the photos to process");
             }
         }
     }
--- src/PhotoStats/Program.cs
+++ src/PhotoStats/Program.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using CommandLine;
-using CommandLine.Text;
 using NMagickWand;
 using NMagickWand.Enums;
 
@@ -23,30 +19,11 @@ namespace PhotoStats
         
         public static void Main(string[] args)
         {
-            var parser = new Parser(config => config.HelpWriter = null);
-            var result = parser.ParseArguments<PhotoStatsOptions>(args);
+            var opts = new PhotoStatsOptions();
+            opts.Parse(args);
             
-            var exitCode = result.MapResult(
-                opts => 
-                {
-                    var errs = opts.ValidateOptions().ToList();
-                    
-                    if(errs.Count > 0)
-                    {
-                        ShowUsage(errs);
-                        return 1;
-                    }
-            
-                    var p = new Program(opts);
-                    p.Run();
-                    return 0;
-                },
-                errors =>
-                {
-                    ShowUsage();
-                    return 1;
-                }
-            );
+            var p = new Program(opts);
+            p.Run();
         }
         
         
@@ -81,31 +58,6 @@ namespace PhotoStats
         void WriteLine(params object[] vals)
         {
             Console.WriteLine(string.Join("\t", vals));
-        }
-        
-        
-        static void ShowUsage(IList<string> errors = null)
-        {
-            var help = new HelpText();
-            
-            help.Heading = "PhotoStats";
-            help.AddPreOptionsLine("A tool to suggest optimization parameters for SizePhotos");
-            
-            // this is a little lame, but force a NotParsed<T> options result
-            // so that we can get a nice help screen.  this might be required
-            // if the passed args are valid to the parser, but not w/ custom 
-            // validation logic that runs after parsing
-            var parser = new Parser(config => config.HelpWriter = null);
-            var result = parser.ParseArguments<PhotoStatsOptions>(new string[] { "--xxx" });
-            help.AddOptions(result);
-            
-            if(errors != null)
-            {
-                help.AddPostOptionsLine("Errors:");
-                help.AddPostOptionsLines(errors);
-            }
-            
-            Console.WriteLine(help.ToString());            
         }
     }
 }
